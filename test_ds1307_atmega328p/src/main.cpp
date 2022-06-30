9a31d5b764d973e32ea1931100fd5354a463d38a
caonhduy <107563666+caonhduy@users.noreply.github.com>
2022-06-30 11:39:27 +0700

    rewrite again lmao

--- test_ds1307_atmega328p/src/main.cpp
+++ test_ds1307_atmega328p/src/main.cpp
@@ -1,35 +1,48 @@
 #include<Arduino.h>
-#include<rtc.h>
+#include<Eventually.h>
 
-#include "LiquidCrystal_I2C.h"
+#include"LiquidCrystal_I2C.h"
 
-/* FIXME stub pin numbers */
-#define PIN_BUTTON_0 B0
-#define PIN_BUTTON_1 B1
-#define PIN_BUTTON_2 B2
+#define PIN_BUTTON_0 0
+#define PIN_BUTTON_1 1
+#define PIN_BUTTON_2 2
 
 #define PIN_HUMIDITY A3
-#define PIN_VALVE    9
-#define ADDRESS_RTC  0x68
+#define PIN_VALVE     9
 
-#define HUMIDITY_CRITICAL 25;
-#define HUMIDITY_GOOD     70;
+static const int HUMIDITY_CRITICAL = 25;
+static const int HUMIDITY_GOOD     = 70;
 
-static uint8_t schedule_0_hour   = 9;
-static uint8_t schedule_0_minute = 0;
-static uint8_t schedule_1_hour   = 15;
-static uint8_t schedule_1_minute = 0;
+static int humidity;
 
-enum AutowateringTimeSetAction {
-	NOP,
-	HOUR_0,
-	HOUR_1,
-	MINUTE_0,
-	MINUTE_1,
-	ACTION_BOUNDARY
-};
+static int mode              = 0;
+static int schedule_0_hour   = 9;
+static int schedule_0_minute = 0;
+static int schedule_1_hour   = 15;
+static int schedule_1_minute = 0;
 
-LiquidCrystal_I2C lcd(0x27,16,2);
+static const int SECOND = 0;
+static const int MINUTE = 1;
+static const int HOUR   = 2;
+static const int DATE   = 3;
+static const int DAY    = 4;
+static const int MONTH  = 5;
+static const int YEAR   = 6;
+
+static const int NOP             = 0;
+static const int HOUR_0          = 1;
+static const int HOUR_1          = 2;
+static const int MINUTE_0        = 3;
+static const int MINUTE_1        = 4;
+static const int ACTION_BOUNDARY = 5;
+
+static LiquidCrystal_I2C lcd(0x27,16,2);
+
+static EvtManager mgr;
+
+static EvtListener * display;
+static EvtListener * toggle;
+static EvtListener * button[3];
 
 void
 setup()
@@ -45,100 +58,69 @@ setup()
 	ds1307_save(MINUTE,0x59);
 	ds1307_save(SECOND,0x50);
 	ds1307_save(DATE,2);
+
 	pinMode(PIN_HUMIDITY,INPUT);
 	pinMode(PIN_VALVE,OUTPUT);
 	pinMode(PIN_BUTTON_0,INPUT);
 	pinMode(PIN_BUTTON_1,INPUT);
 	pinMode(PIN_BUTTON_2,INPUT);
+
 	digitalWrite(PIN_VALVE,LOW);
+
+	display   = new EvtTimeListener(200,true,(EvtAction)lcd_output);
+	toggle    = new EvtTimeListener(1000,true,(EvtAction)valve_control);
+	button[0] = new EvtPinListener(PIN_BUTTON_0,(EvtAction)mode_change);
+	button[1] = new EvtPinListener(PIN_BUTTON_1,(EvtAction)time_increase);
+	button[2] = new EvtPinListener(PIN_BUTTON_2,(EvtAction)time_decrease);
+
+	mgr.addListener(display);
+	mgr.addListener(button[0]);
 }
 
-static uint8_t
-bcd(uint8_t z)
+int
+bcd(int z)
 {
 	return ((z / 10) << 4) + (z % 10);
 }
 
-static String
-bcd_to_string(uint8_t d)
+String
+bcd_to_string(int d)
 {
 	return String(d >> 4) + String(d & 0x0f);
 }
 
-static void
-autowatering_time_set_value(enum DS1037Datetime type, uint8_t *value)
+int
+ds1307_load(int type)
 {
-	uint8_t limit;
-
-	/* TODO add more limits */
-	switch (type) {
-	case MINUTE:
-		*limit = 60;
-		break;
-	case HOUR:
-		*limit = 24;
-		break;
-	default:
-		*limit = 0;
+	Wire.beginTransmission(0x68);
+	Wire.write(type);
+	Wire.endTransmission();
+	Wire.requestFrom(0x68,1);
+	if (type == HOUR) {
+		return Wire.read() & 0x3f;
 	}
-
-	if (digitalRead(PIN_BUTTON_0) == HIGH) {
-		return;
-	}
-	if (digitalRead(PIN_BUTTON_1) == HIGH) {
-		if (*value < limit) {
-			*value = *value + 1;
-		}
-	}
-	if (digitalRead(PIN_BUTTON_2) == HIGH) {
-		if (*value > 0) {
-			*value = *value - 1;
-		}
-	}
-}
-
-static void
-autowatering_time_set(void)
-{
-	static enum AutowateringTimeSetAction a = NOP;
-
-	if (digitalRead(PIN_BUTTON_0) == HIGH) {
-		a = (a + 1) % ACTION_BOUNDARY;
-	}
-	switch (a) {
-	case NOP:
-		break;
-	case HOUR_0:
-		lcd.setCursor(0,0);
-		lcd.print("Gio tuoi 1: " + schedule_0_hour);
-		autowatering_time_set_value(HOUR,&schedule_0_hour);
-		break;
-	case MINUTE_0:
-		lcd.setCursor(0,0);
-		lcd.print("Phut tuoi 1: " + schedule_0_minute);
-		autowatering_time_set_value(MINUTE,&schedule_0_minute);
-		break;
-	case HOUR_1:
-		lcd.setCursor(0,0);
-		lcd.print("Gio tuoi 2: " + schedule_1_hour);
-		autowatering_time_set_value(HOUR,&schedule_1_hour);
-		break;
-	case MINUTE_1:
-		lcd.setCursor(0,0);
-		lcd.print("Phut tuoi 2: " + schedule_1_minute);
-		autowatering_time_set_value(MINUTE,&schedule_1_minute);
-	}
+	return Wire.read();
 }
 
 void
-loop()
+ds1307_save(int type, int value)
 {
-	uint8_t humidity = map(analogRead(PIN_HUMIDITY),0,1023,100,0);
+	Wire.beginTransmission(0x68);
+	Wire.write(type);
+	if (type == HOUR) {
+		Wire.write(value | 0x40);
+	} else {
+		Wire.write(value);
+	}
+	Wire.endTransmission();
+}
 
-	bool is_critical = ds1307_load(SECOND) <= 2 && humidity < HUMIDITY_CRITICAL;
-	bool on_hour_0   = ds1307_load(HOUR)   == bcd(schedule_0_hour);
+bool
+valve_control(void)
+{
+	bool on_hour_0   = ds1307_load(HOUR)	 == bcd(schedule_0_hour);
 	bool on_minute_0 = ds1307_load(MINUTE) == bcd(schedule_0_minute);
-	bool on_hour_1   = ds1307_load(HOUR)   == bcd(schedule_0_hour);
+	bool on_hour_1   = ds1307_load(HOUR)	 == bcd(schedule_0_hour);
 	bool on_minute_1 = ds1307_load(MINUTE) == bcd(schedule_0_minute);
 
 	bool on_schedule_0 = on_hour_0 && on_minute_0;
@@ -146,21 +128,95 @@ loop()
 
 	bool on_schedule = on_schedule_0 || on_schedule_1;
 
-	lcd.setCursor(0,0);
-	lcd.print(bcd_to_string(rtc_load(HOUR))
-	  + ":" + bcd_to_string(rtc_load(MINUTE))
-	  + ":" + bcd_to_string(rtc_load(SECOND));
-	lcd.setCursor(0,1);
-	lcd.print("Do am: " + String(humidity) + "%");
+	humidity = map(analogRead(PIN_HUMIDITY),0,1023,100,0);
 
-	autowatering_time_set();
-
-	/* FIXME no sleeping mode */
-	if (is_critical || (on_schedule && humidity < HUMIDITY_GOOD)) {
+	if (humidity < HUMIDITY_CRITICAL || (on_schedule && humidity < HUMIDITY_GOOD)) {
 		digitalWrite(PIN_VALVE,HIGH);
 	} else {
 		digitalWrite(PIN_VALVE,LOW);
 	}
 
-	delay(200);
+	return false;
 }
+
+bool
+lcd_output(void)
+{
+	switch (mode) {
+	case NOP:
+		lcd.setCursor(0,0);
+		lcd.print(bcd_to_string(ds1307_load(HOUR))
+		  + ":" + bcd_to_string(ds1307_load(MINUTE))
+		  + ":" + bcd_to_string(ds1307_load(SECOND)));
+		lcd.setCursor(0,1);
+		lcd.print("Do am: " + String(humidity) + "%");
+		break;
+	case HOUR_0:
+		lcd.setCursor(0,0);
+		lcd.print("Gio tuoi 1: " + schedule_0_hour);
+		break;
+	case MINUTE_0:
+		lcd.setCursor(0,0);
+		lcd.print("Phut tuoi 1: " + schedule_0_minute);
+		break;
+	case HOUR_1:
+		lcd.setCursor(0,0);
+		lcd.print("Gio tuoi 2: " + schedule_1_hour);
+		break;
+	case MINUTE_1:
+		lcd.setCursor(0,0);
+		lcd.print("Phut tuoi 2: " + schedule_1_minute);
+	}
+	return true;
+}
+
+bool
+mode_change(void)
+{
+	mode = (mode + 1) % ACTION_BOUNDARY;
+	return true;
+}
+
+bool
+time_increase(void)
+{
+	switch (mode) {
+	case NOP:
+		break;
+	case HOUR_0:
+		schedule_0_hour = (schedule_0_hour + 1) % 24;
+		break;
+	case MINUTE_0:
+		schedule_0_minute = (schedule_0_minute + 1) % 60;
+		break;
+	case HOUR_1:
+		schedule_1_hour = (schedule_1_hour + 1) % 24;
+		break;
+	case MINUTE_1:
+		schedule_1_minute = (schedule_1_minute + 1) % 60;
+	}
+	return true;
+}
+
+bool
+time_decrease(void)
+{
+	switch (mode) {
+	case NOP:
+		break;
+	case HOUR_0:
+		schedule_0_hour = (schedule_0_hour - 1) % 24;
+		break;
+	case MINUTE_0:
+		schedule_0_minute = (schedule_0_minute - 1) % 60;
+		break;
+	case HOUR_1:
+		schedule_1_hour = (schedule_1_hour - 1) % 24;
+		break;
+	case MINUTE_1:
+		schedule_1_minute = (schedule_1_minute - 1) % 60;
+	}
+	return true;
+}
+
+USE_EVENTUALLY_LOOP(mgr)
